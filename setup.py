89c61f4580b491b81248d9f9c1b5afac3521f77b
Prathamesh Bhujbal <bhujbalprathamesh8@gmail.com>
2025-02-18 21:34:57 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="GenAI Project",
+    version="0.0.0",
+    author='Prathamesh B',
+    author_email='bhujbalprathamesh8@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
